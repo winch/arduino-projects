79bcb95ab63ee392345dafd408ef182ee55f1770
david <david@4a3fcead-100c-0410-8434-c0d6b4fee585>
2008-06-21 23:22:52 +0000

    now reads input from joystick and sends it over serial
    git-svn-id: http://crab/source/arduino@354 4a3fcead-100c-0410-8434-c0d6b4fee585

---
+++ rc_car/controller/axis.rb
@@ -0,0 +1,52 @@
+
+class Axis
+    
+    def initialize(ident, serial, centre, min, max, reverse)
+        @ident = ident
+        @sp = serial
+        @output_centre = centre
+        @output_min = min
+        @output_max = max
+        @output_range = max - min
+        @min = 0
+        @max = 0
+        @current = 0
+        @diff = 100
+        @reverse = reverse
+    end
+    
+    car = 1000
+    joy = 500
+    
+    #update the axis if required
+    def update(value)
+        value = value * -1 if @reverse == true
+        @max = value if value > @max
+        @min = value if value < @min
+        
+        if (@current - value).abs > @diff
+            @current = value
+            send
+        end
+    end
+    
+    #send value over serial port
+    def send
+        if @sp.nil? == false
+            range = @max - @min
+            scale = @output_range / range.to_f
+            value = (@current * scale) + @output_centre
+            value = value.to_i.to_s
+            
+            #send over serial
+            @sp.putc(@ident)
+            value.each_byte do |char|
+                @sp.putc(char)
+            end
+            @sp.putc('e')
+            puts "sent #{@ident}#{value}"
+        end
+    end
+    
+end
+
--- rc_car/controller/controller.rb
+++ rc_car/controller/controller.rb
@@ -1,23 +1,41 @@
 #!/usr/bin/ruby -w
 
-require 'serialport.so'
+require 'serialport'
+require 'joystick'
+require File.dirname(__FILE__) + '/axis.rb'
 
 if ARGV.size < 1
-    puts 'Usage ./controller.rb serialPort'
+    puts 'Usage ./controller.rb serialPort joystick'
     exit
 end
 
-sp = SerialPort.new(ARGV[0], 9600, 8, 1, SerialPort::NONE)
+STEER_AXIS = 0
+THROTTLE_AXIS = 2
 
-sp.putc('a')
-sleep(5)
-sp.putc('b')
-sleep(1)
-sp.putc('r')
-sleep(1)
-sp.putc('b')
-sleep(5)
-sp.putc('r')
+
+sp = SerialPort.new(ARGV[0], 38400, 8, 1, SerialPort::NONE)
+js = Joystick::Device::open ARGV[1]
+
+throttle = Axis.new('t', sp, 1500, 1000, 2000, true)
+steer = Axis.new('s', sp, 1500, 1240, 1860, false)
+
+go = true
+while go == true
+    event = js.next_event
+    
+    case event.type
+    when Joystick::Event::BUTTON
+        go = false if event.num == 0 && event.val == 1
+    when Joystick::Event::AXIS
+        if event.num == STEER_AXIS
+            steer.update(event.val)
+        end
+        if event.num == THROTTLE_AXIS
+            throttle.update(event.val)
+        end
+    end
+end
 
 sp.close
+js.close
 
