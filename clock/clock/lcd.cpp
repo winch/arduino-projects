f96757bb478d7867cbe598f0ef4b7d5e939d3d04
david <david@4a3fcead-100c-0410-8434-c0d6b4fee585>
2008-08-24 10:40:21 +0000

    Lcd, don't blank the line before printing. Avoids slight flickering.
    git-svn-id: http://crab/source/arduino@373 4a3fcead-100c-0410-8434-c0d6b4fee585

--- clock/clock/lcd.cpp
+++ clock/clock/lcd.cpp
@@ -8,6 +8,7 @@
 
 static LCD4Bit lcd = LCD4Bit(LINES);
 static char lcd_line[LINE_WIDTH + 1];
+static char blank_line[] = "                ";
 
 void lcd_print(byte, char);
 
@@ -22,20 +23,17 @@ void lcd_print(byte line, char *msg)
 {
   byte i;
   lcd.cursorTo(line + 1, 0);
-  for(i = 0; i < LINE_WIDTH; i++)
-    lcd.printIn(" ");
-  lcd.cursorTo(line + 1, 0);
   lcd.printIn(msg);
 }
 
 void lcd_print_time(byte line, rtc_time *time)
 {
-  snprintf(lcd_line, LINE_WIDTH, "%.2d:%.2d:%.2d%s", time->hour, time->minute, time->second, rtc_period[time->period]);
+  snprintf(lcd_line, LINE_WIDTH, "%.2d:%.2d:%.2d%s%s", time->hour, time->minute, time->second, rtc_period[time->period], blank_line);
   lcd_print(line, lcd_line);
 }
 
 void lcd_print_date(byte line, rtc_time *time)
 {
-  snprintf(lcd_line, LINE_WIDTH, "%s %.2d-%.2d-%d", rtc_days[time->w_day], time->day, time->month, time->year);
+  snprintf(lcd_line, LINE_WIDTH, "%s %.2d-%.2d-%d%s", rtc_days[time->w_day], time->day, time->month, time->year, blank_line);
   lcd_print(line, lcd_line);
 }
